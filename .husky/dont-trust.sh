d2ad3516f2388561375fdb7f6e04ae2ba5faeeda
sebastianlujan <sebastianlujan@proton.me>
2024-12-02 21:56:25 -0300

    build(dont-trust): add signing verification for commits

--- .husky/dont-trust.sh
+++ .husky/dont-trust.sh
@@ -1,1 +1,14 @@
-# verify if the commit is signed, why not?
+# Verify if the commit is signed, why not?
+
+# -1 is the first log message
+# G means "good valid signature", N no signature, https://git-scm.com/docs/git-log#Documentation/git-log.txt-emGem
+
+
+VERIFY_SIGNING=$(git log -1 --pretty=%G? | grep -q "G")
+
+if [ ! $VERIFY_SIGNING != "G" ] || [ ! $VERIFY_SIGNING != "E" ]; then
+    echo "Error: Commit is not signed 💔. Please sign your commit."
+    exit 1
+fi
+
+echo "Commit signed! ❤️"
