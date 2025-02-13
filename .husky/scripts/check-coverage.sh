2909ebaa1c17d87958a14d7f6644af33c30b4af7
sebastianlujan <1333184+sebastianlujan@users.noreply.github.com>
2025-02-13 02:14:28 -0300

    feat(workflow): :sparkles: add coverage and testing scripts

---
+++ .husky/scripts/check-coverage.sh
@@ -0,0 +1,32 @@
+MIN_COVERAGE=70
+
+check_coverage() {
+    echo "🧪 Checking test coverage..."
+
+    # Get the coverage number from the output of sui move coverage summary
+    local coverage=$(sui move coverage summary | awk '/Move Coverage:/ {print $5}')
+
+    # Check if the coverage is greater than or equal to the minimum required
+    # Awk returns 0 if the condition is false, so we check if the result is not 0
+    
+    # Compare values using AWK
+    awk -v cov="$coverage" -v min="$MIN_COVERAGE" '
+        BEGIN {
+            if (cov >= 90 ) {
+                printf "%s🌟 Excellent coverage: %.1f%% (Minimum: %d%%)%s\n", 
+                green, cov, min, nc
+            } else if (cov >= 80) {
+                printf "%s✨ Good coverage: %.1f%% (Minimum: %d%%)%s\n", 
+                yellow, cov, min, nc
+            } if (cov >= min) {
+                printf "✅ Coverage: %.1f%% (Minimum: %d%%)\n", cov, min
+                exit 0
+            } else {
+                printf "❌ Coverage failed: %.1f%% < %d%%\n", cov, min
+            }
+        }'
+    exit $?
+}
+
+
+check_coverage
---
+++ .husky/scripts/dont-trust.sh
@@ -0,0 +1,21 @@
+# Verify if the commit is signed, why not?
+
+# -1 is the first log message
+# G means "good valid signature", N no signature, https://git-scm.com/docs/git-log#Documentation/git-log.txt-emGem
+
+dont_trust() {
+    echo "🔏 Verifying commit signature..."
+
+    VERIFY_SIGNING=$(git log -1 --pretty=%G?)
+
+    if [[ "$VERIFY_SIGNING" != 'G' && "$VERIFY_SIGNING" != 'E' ]]; then
+        echo "❌ Error: Commit is not signed"
+        echo "Please sign your commit."
+        exit 1
+    fi
+
+    echo "Commit signed! 🔑"
+
+}
+
+dont_trust
